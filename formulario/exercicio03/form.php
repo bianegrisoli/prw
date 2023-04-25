bcf58bfb528c99aecfa26f9f5b9820a94b5ebf89
bianegrisoli <bianca.negrisoli@aluno.ifsp.edu.br>
2023-04-25 12:25:24 -0300

    formulario

---
+++ formulario/exercicio03/form.php
@@ -0,0 +1,69 @@
+<?php
+$estado=$_POST['estado'];
+
+if ("Acre"){
+    echo "Rio Branco";
+}
+
+else if ("Alagoas"){
+    echo "Maceió";
+}
+
+else if ("Amapá"){
+    echo "Macapá";
+}
+
+else if ("Amazonas"){
+    echo "Manaus";
+}
+
+else if ("Bahia"){
+    echo "Salvador";
+}
+
+else if ("Ceará"){
+    echo "Fortaleza";
+}
+
+else if ("Distrito Federal"){
+    echo "Brasília";
+}
+
+else if ("Espírito Santo"){
+    echo "Vitória";
+}
+
+else if ("Goiás"){
+    echo "Goiânia";
+}
+
+else if ("Maranhão"){
+    echo "São Luís";
+}
+
+else if ("Mato Grosso"){
+    echo "Cuiabá";
+}
+
+else if ("Mato Grosso do Sul"){
+    echo "Campo Grande";
+}
+
+else if ("Minas Gerais"){
+    echo "Belo Horizonte";
+}
+
+else if ("Pará"){
+    echo "Belém";
+}
+
+else if ("Paraíba"){
+    echo "João Pessoa";
+}
+
+else if ("Paraná"){
+    echo "Curitiba";
+}
+
+
+?>
